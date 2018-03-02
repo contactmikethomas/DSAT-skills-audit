c6fbc5de5d7490c291528e4d55e39ec3a26fc863
contactmikethomas <36892220+contactmikethomas@users.noreply.github.com>
2018-03-02 11:49:19 +0000

    Update hello.py

--- python/hello.py
+++ python/hello.py
@@ -15,7 +15,7 @@ data = json.loads(os.getenv("VCAP_SERVICES"))
 @app.route('/')
 def hello_world():
 	try:
-		cnx = mysql.connector.connect(user=data['mysql'][0]['credentials']['username'], password=data['mysql'][0]['credentials']['password'],host=data['mysql'][0]['credentials']['host'],port=json.dumps(data['mysql'][0]['credentials']['port']),ssl_ca='/etc/ssl/certs/',ssl_verify_cert=True)
+		cnx = mysql.connector.connect(user=data['mysql'][0]['credentials']['username'], password=data['mysql'][0]['credentials']['password'],host=data['mysql'][0]['credentials']['host'],port=json.dumps(data['mysql'][0]['credentials']['port']),ssl_ca='/etc/ssl/certs/Amazon_Root_CA_1.pem',ssl_verify_cert=True)
 		cursor = cnx.cursor(buffered=True)
 		cursor.execute("USE SurveyResults")
 		cursor.execute("SELECT * FROM SurveyResults")
