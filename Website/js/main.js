258f3a3a650354e770eccd2ccc1e5c5642a8eb39
contactmikethomas <36892220+contactmikethomas@users.noreply.github.com>
2018-03-02 11:26:47 +0000

    These are the real files (Part 3)

---
+++ Website/js/main.js
@@ -0,0 +1,38 @@
+
+(function ($) {
+	"use strict";
+	$('.column100').on('mouseover',function(){
+		var table1 = $(this).parent().parent().parent();
+		var table2 = $(this).parent().parent();
+		var verTable = $(table1).data('vertable')+"";
+		var column = $(this).data('column') + ""; 
+
+		$(table2).find("."+column).addClass('hov-column-'+ verTable);
+		$(table1).find(".row100.head ."+column).addClass('hov-column-head-'+ verTable);
+	});
+
+	$('.column100').on('mouseout',function(){
+		var table1 = $(this).parent().parent().parent();
+		var table2 = $(this).parent().parent();
+		var verTable = $(table1).data('vertable')+"";
+		var column = $(this).data('column') + ""; 
+
+		$(table2).find("."+column).removeClass('hov-column-'+ verTable);
+		$(table1).find(".row100.head ."+column).removeClass('hov-column-head-'+ verTable);
+	});
+    
+
+    [ Chose Radio ]*/
+    $("#radio1").on('change', function(){
+        if ($(this).is(":checked")) {
+            $('.input3-select').slideUp(300);
+        }
+    });
+
+    $("#radio2").on('change', function(){
+        if ($(this).is(":checked")) {
+            $('.input3-select').slideDown(300);
+        }
+    });
+
+})(jQuery);
